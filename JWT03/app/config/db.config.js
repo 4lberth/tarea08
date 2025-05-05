eaaa8cf304187fb00c0b7804da8010904c854c2d
Alberth Mayta Cuevas <alberth.mayta@tecsup.edu.pe>
2025-05-05 11:46:43 -0500

    configurando las variables 3

--- JWT03/app/config/db.config.js
+++ JWT03/app/config/db.config.js
@@ -1,14 +1,18 @@
 export default {
-    HOST: "interchange.proxy.rlwy.net",
-    USER: "root",
-    PASSWORD: "AGiQiNGnjOtuXAkWuwjoAyibqxSAeZmt",
-    DB: "railway",
-    PORT: 34652,
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASS,
+    DB: process.env.DB_NAME,
+    PORT: process.env.DB_PORT,
     dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
+  
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true
+      }
     },
-};
+  
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
+  };
+  
